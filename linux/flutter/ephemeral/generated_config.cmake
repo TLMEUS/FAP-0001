4da30e756459c3b77bc89c6c298d5b709e3550ff
Troy Marker <owner@troylmarkerenterprises.com>
2024-10-03 10:42:36 -0500

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/tlme/development/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/tlme/TLME Projects/FAP-0001" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/tlme/development/flutter"
+  "PROJECT_DIR=/home/tlme/TLME Projects/FAP-0001"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/tlme/TLME Projects/FAP-0001/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
